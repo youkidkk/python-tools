0462e7ec6adb2c0fe5949631e911c9463adbd7ce
youkidkk <youkidkk80@gmail.com>
2019-06-23 10:15:51 +0900

    初期コミット

---
+++ ykdpytool/auto_renamer.py
@@ -0,0 +1,130 @@
+import argparse
+import os
+import threading
+from time import sleep
+
+from ykdpyutil.file import files
+
+target_dir = ""
+dest_dir = ""
+interval = 1
+length = 3
+
+running = True
+
+
+def get_args() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target_dir")
+    parser.add_argument("-d", "--dest", type=str)
+    parser.add_argument("-i", "--interval", type=int, default=1)
+    parser.add_argument("-l", "--length", type=int, default=2)
+    args = parser.parse_args()
+
+    global target_dir, dest_dir, interval, length
+    target_dir = args.target_dir
+    dest_dir = args.dest or target_dir
+    interval = args.interval
+    length = args.length
+
+
+def check_dir(path: str) -> bool:
+    if not os.path.exists(path):
+        print("'{}' is not exists. : ".format(path))
+        return False
+    if os.path.isfile(path):
+        print("'{}' is not directory.".format(path))
+        return False
+    if len(files.get_paths(path)) > 1:
+        print("'{}' is not empty.".format(path))
+        return False
+    return True
+
+
+def wait_update(target: str) -> None:
+    cnt = 0
+    size_last = 0
+    while True:
+        size = os.path.getsize(target)
+        if size_last == size:
+            cnt += 1
+        else:
+            size_last = size
+            cnt = 0
+        if cnt >= 5:
+            return
+        sleep(interval)
+
+
+def move(target: str) -> None:
+    prefix, suffix = files.get_prefix_suffix(target)
+    if prefix is None:
+        return
+
+    wait_update(target)
+
+    max = int("9" * length)
+    for idx in range(1, max):
+        dst = os.path.join(dest_dir, prefix, "{}-{}.{}"
+                           .format(prefix, str(idx).zfill(length), suffix))
+        if not os.path.exists(dst):
+            while True:
+                try:
+                    files.move(target, dst)
+                    break
+                except PermissionError:
+                    files.delete(dst)
+                sleep(interval)
+            msg = "\r'{}' -> '{}'".format(
+                os.path.basename(target),
+                os.path.basename(dst))
+            print(msg)
+            return None
+
+
+def end_process() -> None:
+    for file in files.get_files(target_dir):
+        files.move(file, os.path.join(target_dir, os.path.basename(file)))
+    for dir in reversed(files.get_dirs(target_dir)):
+        os.rmdir(dir)
+
+
+def main_loop() -> None:
+    cnt = 1
+    max = 10
+    while True:
+        if not running:
+            end_process()
+            print("Finish!!!")
+            break
+        print("\rRunning" + ("." * cnt).ljust(max), end="")
+        cnt += 1
+        if cnt > max:
+            cnt -= max
+        targets = files.get_files(
+            target_dir, lambda p: os.path.dirname(p) == target_dir)
+        for target in targets:
+            try:
+                move(target)
+            except FileNotFoundError:
+                pass
+        sleep(interval)
+
+
+def main() -> None:
+    get_args()
+    if not check_dir(target_dir) or not check_dir(dest_dir):
+        return
+    print("Please press <Enter-key> to finish.")
+
+    global running
+    thread = threading.Thread(target=main_loop)
+    thread.start()
+
+    input()
+    running = False
+    thread.join()
+
+
+if __name__ == '__main__':
+    main()
---
+++ ykdpytool/photo_renamer.py
@@ -0,0 +1,96 @@
+import argparse
+import os
+from datetime import datetime
+
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+from ykdpyutil.file import files
+from ykdpyutil.time import datetimes
+
+
+def get_exif(file):
+    im = Image.open(file)
+
+    try:
+        exif = im._getexif()
+        exif_table = {}
+        for tag_id, value in exif.items():
+            tag = TAGS.get(tag_id, tag_id)
+            exif_table[tag] = value
+        return exif_table
+    except AttributeError:
+        return None
+
+
+def get_datetime_org_text(file):
+    exif_table = get_exif(file)
+    if exif_table is not None:
+        dt = exif_table.get("DateTimeOriginal")
+        ms = exif_table.get("SubsecTimeOriginal") or "000000"
+
+        if datetime:
+            return "{}.{}".format(dt, ms)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("src_root")
+    parser.add_argument("dst_root")
+    parser.add_argument("-p", "--pattern", type=str,
+                        default="%Y-%m%d_%H%M%S-%f")
+    args = parser.parse_args()
+
+    return args.src_root, args.dst_root, args.pattern
+
+
+def get_datetime(file):
+    result = datetimes.get_from_str(
+        get_datetime_org_text(file), "%Y:%m:%d %H:%M:%S.%f")
+    if result is None:
+        print("\n{} : EXIF情報が取得できないため、作成日時にてファイル名を設定します。 ".format(file))
+        result = files.get_created(file)
+    return result
+
+
+def get_dst_path(dst_parent, filename):
+    dst_path = os.path.join(dst_parent, filename + ".jpg")
+    if os.path.exists(dst_path):
+        idx = 1
+        while os.path.exists(dst_path):
+            dst_path = os.path.join(
+                dst_parent, filename + "-" + str(idx) + ".jpg")
+            idx += 1
+        print("\n{} : 出力先パスが存在するため、連番を付与しました。  ".format(dst_path))
+    return dst_path
+
+
+def main():
+    src_root, dst_root, pattern = get_args()
+
+    files.clear_dir(dst_root)
+
+    list = files.get_files(
+        src_root, lambda p: files.get_suffix(p).lower() == "jpg")
+    count = 1
+    total = len(list)
+    for src_path in list:
+        rel_path = os.path.relpath(src_path, src_root)
+        dst_parent = os.path.dirname(os.path.join(dst_root, rel_path))
+
+        dt = get_datetime(src_path)
+
+        filename = dt.strftime(pattern)
+        dst_path = get_dst_path(dst_parent, filename)
+
+        files.copy(src_path, dst_path)
+        files.modify_times(dst_path, dt)
+
+        print(("\r{:>" + str(len(str(total))) +
+               "}/{} 完了しました。 {}").format(count, total, str(dst_path)), end="")
+        count += 1
+    print("\n終了しました。")
+
+
+if __name__ == '__main__':
+    main()
