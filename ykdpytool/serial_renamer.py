0c1a160c3d51af76d531b03e60103fac57f5da69
youkidkk <youkidkk80@gmail.com>
2019-09-04 00:18:03 +0900

    コメント追加など

--- ykdpytool/serial_renamer.py
+++ ykdpytool/serial_renamer.py
@@ -26,33 +26,47 @@ def max_len(text_list):
 
 
 def rename(target_dir):
+    # 対象ディレクトリ直下のファイルパスのリストを取得
     path_list = files.get_files(target_dir, recursive=False)
+
+    # ファイル名のリストを取得
     file_list = list(map(lambda p: os.path.basename(p), path_list))
+
     if not file_list:
+        # 該当ファイルなしの場合は終了
         print("Error: 対象ファイルなし")
         return
+
+    # 先頭の一致部分の取得
     starts = same_text(file_list)
+
+    # 末尾の一致部分の取得
     reversed_list = list(map(lambda i: texts.reverse(i), file_list))
     ends = texts.reverse(same_text(reversed_list))
 
-    print(starts, ends)
+    print("先頭一致部分： {}".format(starts))
+    print("末尾一致部分： {}".format(ends))
 
+    # 不一致部分のリストを取得
     df_list = list(
         map(lambda p: p[len(starts): len(p) - len(ends)], file_list))
-    if all(list(map(lambda d: re.match("[0-9]+", d), df_list))):
-        if starts == "0":
-            print("Error: リネーム不要のため中断")
-            return
-        num_len = max_len(df_list) + 1
-        for idx, src in enumerate(file_list):
-            dst = df_list[idx].zfill(num_len) + ends
-            print(src, "->", dst)
-            src_path = os.path.join(target_dir, src)
-            dst_path = os.path.join(target_dir, dst)
-            files.move(src_path, dst_path)
-    else:
+
+    if not all(list(map(lambda d: re.match("[0-9]+", d), df_list))):
         print("Error: 不一致部分に数値以外が含まれている")
         return
+
+    if starts == "0":
+        print("Error: リネーム不要のため中断")
+        return
+
+    num_len = max_len(df_list) + 1
+    for idx, src in enumerate(file_list):
+        dst = df_list[idx].zfill(num_len) + ends
+        print(src, "->", dst)
+        src_path = os.path.join(target_dir, src)
+        dst_path = os.path.join(target_dir, dst)
+        if src_path != dst_path:
+            files.move(src_path, dst_path)
 
 
 def main():
